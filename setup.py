c4b2ce4a2f34ecf608d885e4c1102e8b2fadef08
Ryan Weyers <weyers.ryan@gmail.com>
2018-02-20 21:15:01 +1300

    Changed the readme to restructured text and created script for creating python packages

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+ 
+setup(name='compute_pipe',
+      version='0.1',
+      url='https://github.com/Ulfasaar/pipe',
+      license='MIT',
+      author='Ryan Weyers',
+      author_email='weyers.ryan@gmail.com',
+      description='A beautiful, simple, fast framework for creating computation pipelines.',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.rst').read(),
+      zip_safe=True)
